bc49f7925d5c22a3c12999487119dbda8261baaa
Jarek Wyprzal <jwyprzal@gmail.com>
2023-06-03 21:20:25 +0100

    map questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
+import { useState } from 'react'
+import questions from './data'
 const App = () => {
-  return <h2>Accordion Starter</h2>;
-};
-export default App;
+  const [question, setQuestion] = useState(questions)
+  return (
+    <main>
+      <section className='container'>
+        <h1>Questions</h1>
+        {question.map(question => {
+          const { id, title, info } = question
+          return (
+            <article className='question' key={id}>
+              <header>
+                <h5>{title}</h5>
+                <button className='question-btn'>+</button>
+              </header>
+              <p>{info}</p>
+            </article>
+          )
+        })}
+      </section>
+    </main>
+  )
+}
+export default App
